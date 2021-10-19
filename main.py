25673463b9d43c75243d0956ddccd769d6c8d629
JhonatanRian <jhonatanrian.jr@gmail.com>
2021-10-19 12:19:37 -0300

    Projeto Bot bet-365

--- main.py
+++ main.py
@@ -1,62 +1,194 @@
 """
 """
-import os
+from os import  system
 from time import sleep
+from other import Bot
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from prettytable import PrettyTable
 
-def login(driver: webdriver, usuario, senha) -> None:
-    print("»»» Loguin em andamento")
-    var = None
-    driver.get("https://www.bet365.com/#/HO/")
-    driver.set_page_load_timeout(10)
+def traco() -> None:
+    print(45*"=")
+
+def primeiro_executar(bot, op) -> None:
+    bot.clicar_jogo(op)
     sleep(2)
-    elem = driver.find_elements(By.TAG_NAME, "div")
-    for el in elem:
-        if el.text == "Login":
-            var = el
-            break
-    var.click()
-    elem = driver.find_elements(By.TAG_NAME ,"input")
-    for el in elem:
-        if el.get_attribute("placeholder") == "Usuário":
-            var = el
-            el.send_keys(usuario)
-            break
-    for el in elem:
-        if el.get_attribute("placeholder") == "Senha":
-            el.send_keys(senha)
-            break
-    elem = driver.find_element(By.CLASS_NAME ,"lms-StandardLogin_LoginButton ")
-    elem.click()
-   
-def to_check_loguin(driver: webdriver, user) -> bool:
-    sleep(3)
-    check = False
-    compare = driver.find_elements(By.ID, "body > div:nth-child(1) > div > div.wc-WebConsoleModule_SiteContainer > div.wc-WebConsoleModule_Header > div > div:nth-child(1) > div > div > div.um-Header > div.um-Header_InfoRow > div.um-Header_LeftSideWrapper > div > div > span.um-UserInfo_UserName")
-    for ele in compare:
-        if ele.text == user:
-            print(ele.text)
-            print(True)
-            check = True
-    return check
-    
-def painel():
-    menu = """"
- MENU
- 1.
- 2.
- 3.
- 4.
-"""
+    bot.clicar_time()
+    sleep(2)
+    bot.escrever_valor_aposta()
+    bot.apostar()
+    sleep(2)
+    bot.concluir_aposta()
+    sleep(2)
+    bot.minhas_apostas()
+    sleep(2)
+    bot.encerra_aposta()
+    sleep(2)
+    bot.voltar_minhas_apostas()
 
-os.system("clear")
-user = input("Nome de Usuário: ")
-password = input("Senha: ")
-with webdriver.Firefox() as driver:
-    login(driver, user, password)
-    to_check_loguin(driver, user)
-    #os.system("clear")
-    print("entrando...")
-    while (painel := input(f"{menu}\n»»» ") != "0"):
-        ...
+def mostrar_jogos(jogos: webdriver.Firefox) -> None:
+    x = PrettyTable()
+    lista_jogos = []
+    for jogo in jogos:
+        lista_jogos.append(jogo.text)
+    x.field_names = ["opções", "jogos"]
+    for i in range(len(lista_jogos)):
+        x.add_row([str(i), lista_jogos[i]])
+    x.add_row(["99", "digite 'q' para sair"])
+    print(x)
+
+def loguin(BOT: Bot) -> None:
+    cont = 0
+    while True:
+        cont += 1
+        try:
+            BOT.clica_botao_login()
+            break
+        except:
+            print("\033[1;32mAguarde um pouco...\033[0;0m")
+        if cont == 7:
+            break
+    if cont == 7:
+        print("\033[1;33mAlgo deu errado, por favor tente novamente\033[0;0m")
+        sleep(3)
+        principal()
+    cont = 0
+    while True:
+        cont += 1
+        try:
+            BOT.escrever_dados()
+            BOT.fazer_login()
+            break
+        except:
+            print("\033[1;32mAguarde um pouco...\033[0;0m")
+        if cont == 4:
+            break
+    if cont == 4:
+        print("\033[1;33mAlgo deu errado, por favor tente novamente\033[0;0m")
+        sleep(3)
+        principal()
+
+def painel() -> None:
+    system("cls")
+    titulo = "\033[1;32mBOT-BET365\033[0;0m"
+    traco()
+    print(titulo.center(57))
+    traco()
+    
+def painel_principal() -> dict:
+    system("cls")
+    titulo = "\033[1;32mBOT-BET365\033[0;0m"
+    traco()
+    print(titulo.center(57))
+    traco()
+    usuario = input("\033[1;32mInforme o Usuario:\033[0;0m\n\033[1;33m>>> \033[0;0m").strip()
+    senha = input("\033[1;32mInforme sua senha:\033[0;0m\n\033[1;33m>>> \033[0;0m").strip()
+    time = input("\033[1;32mInforme o time que deseja apostar:\033[0;0m\n\033[1;33m>>> \033[0;0m").strip().title()
+    while True:
+        try:
+            valor = float(input("\033[1;32mInforme o valor da aposta:\033[0;0m\n\033[1;33m>>> \033[0;0m"))
+            break
+        except ValueError:
+            print("\033[1;91mTente adicionar um numero\033[0;0m")
+    valor = str(valor)
+    while True:
+        try:
+            vezes = int(input("\033[1;32mInforme quantas vezes deseja apostar:\033[0;0m\n\033[1;33m>>> \033[0;0m"))
+            break
+        except ValueError:
+            print("\033[0;0mTente adicionar um numero\033[0;0m")
+    dados = {"usuario":usuario, "senha":senha, "time":time, "valor":valor, "vezes":vezes}
+    return dados
+
+def principal() -> None:
+    dados = painel_principal()
+    usuario = dados["usuario"]
+    senha = dados["senha"]
+    time = dados["time"]
+    vezes = dados["vezes"]
+    valor = dados["valor"]
+    bot = Bot(usuario, senha, time, valor)
+    bot.iniciar()
+    sleep(2)
+    bot.abrir_url()
+    sleep(2)
+    painel()
+    print("\033[1;36m>>>O BOT está analizando o site para fazer o login <<<\033[0;0m")
+    loguin(bot)
+    painel()
+    print("\033[1;32mPor favor, feche todas as caixas de alerta, exemplo:\033[0;0m\n\033[1;36m    - atualização de email\n    - verificação de identidade\n    - novas menagens\033[0;0m")
+    print("\n\033[1;32mDepois que que fechar tudo pode continuar. Ou, pressione 'x' para reinscrecrer os dados\033[0;0m")
+    x = input("\033[1;33m>>> \033[0;0m").upper()
+    if x == "X":
+        bot.fechar()
+        principal()
+    painel()
+    print("\033[1;32m>>> Fazendo a pesquisa do time<<< \033[0;0m")
+    very = bot.tela_cheia()
+    count = 0
+    while True:
+        count += 1
+        try:
+            bot.menu_clica()
+            sleep(1)
+            bot.pesquisar_menu()
+            break
+        except:
+            ...
+        if count == 3:
+            break
+    count = 0
+    while True:
+        count += 1
+        try:
+            bot.pesquisar()
+            break
+        except:
+            ...
+        if count == 3:
+            break
+    sleep(1)
+    jogos = bot.capturar_jogos()
+    painel()
+    print("\033[1;32mTabela de opções de jogos, digit o valor que quer escolher\033[0;0m\033[1;33m")
+    mostrar_jogos(jogos)
+    op = int(input(">>> \033[0;0m"))
+    sleep(2)
+    if vezes > 1:
+        for i in range(0, vezes+1):
+            if i == 0:
+                bot.clicar_jogo(op)
+                sleep(2)
+                bot.clicar_time()
+                sleep(2)
+                bot.escrever_valor_aposta()
+                bot.apostar()
+                sleep(2)
+                bot.concluir_aposta()
+                sleep(2)
+                bot.minhas_apostas()
+                sleep(2)
+                bot.encerra_aposta()
+                sleep(2)
+                bot.voltar_minhas_apostas()
+            else:
+                sleep(2)
+                bot.clicar_time()
+                sleep(2)
+                bot.escrever_valor_aposta()
+                bot.apostar()
+                sleep(2)
+                bot.concluir_aposta()
+                sleep(2)
+                bot.minhas_apostas()
+                sleep(2)
+                bot.encerra_aposta()
+                sleep(2)
+                bot.voltar_minhas_apostas()
+    else:
+        primeiro_executar(bot, op)
+
+    bot.fechar()
+    exit()
+    
+if __name__ == '__main__':
+    principal()
--- other.py
+++ other.py
@@ -1,44 +1,6 @@
-import os
-from time import sleep
-from selenium.webdriver.common import action_chains
-from selenium.webdriver.common.action_chains import ActionChains
+import pyautogui as auto
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-def win_act(win_name):
-    import win32gui
-    import re
-
-    class WindowMgr:
-        """Encapsulates some calls to the winapi for window management"""
-
-        def __init__(self):
-            """Constructor"""
-            self._handle = None
-
-        def find_window(self, class_name, window_name=None):
-            """find a window by its class_name"""
-            self._handle = win32gui.FindWindow(class_name, window_name)
-
-        def _window_enum_callback(self, hwnd, wildcard):
-            """Pass to win32gui.EnumWindows() to check all the opened windows"""
-            if re.match(wildcard,
-                         str(win32gui.GetWindowText(hwnd))) is not None:
-                self._handle = hwnd
-
-        def find_window_wildcard(self, wildcard):
-            """find a window whose title matches the wildcard regex"""
-            self._handle = None
-            win32gui.EnumWindows(self._window_enum_callback, wildcard)
-
-        def set_foreground(self):
-            """put the window in the foreground"""
-            win32gui.SetForegroundWindow(self._handle)
-
-    w = WindowMgr()
-    for x in range(3):
-        w.find_window_wildcard(f".*{win_name}*")
-        w.set_foreground()
 
 class Usuario:
     def __init__(self: object, usuario: str, senha: str) -> None:
@@ -62,19 +24,13 @@ class Usuario:
         self.__usuario = novo_usuario
 
 class Bot(Usuario):
-    
-    url: str = "https://www.bet365.com/#/HO/"
         
-    def __init__(self, usuario: str, senha: str, time: str, valor: str, vezes: int) -> None:
+    def __init__(self, usuario: str, senha: str, time: str = "final", valor: float = 1, url: str = "https://www.bet365.com/#/HO/") -> None:
         super().__init__(usuario, senha)
         self.__driver: webdriver.Firefox = None
         self.time: str = time
         self.valor: str = valor
-        self.vezes: int = vezes
-    
-    @classmethod
-    def url(cls):
-        return cls.url
+        self.url: str = url
         
     @property
     def drive(self: object) -> webdriver.Firefox:
@@ -89,35 +45,25 @@ class Bot(Usuario):
     def vezes(self: object, mudar) -> None:
         self.vezes = mudar
         
-    def _init(self: object) -> None:
+    def iniciar(self: object) -> None:
         """Abre o navegador firefox"""
         self.__driver: webdriver.Firefox = webdriver.Firefox()
     
-    def _init_url(self: object) -> None:
+    def abrir_url(self: object) -> None:
         """Abre a url do site"""
         self.__driver.get(self.url)
-        self.__driver.set_page_load_timeout(15)
-        
-    def procura_botao_login(self: object) -> webdriver.Firefox:
-        """
-        Objetivo:
-        - Procurar botão login
-        - retorna um elemento da classe webdrive
-        """
-        sleep(2)
-        element: webdriver.Firefox = self.__driver.find_elements(By.TAG_NAME, "div")
-        for el in element:
-            if el.text == "Login":
-                var = el
-                return var
+        self.__driver.set_page_load_timeout(5)
     
     def clica_botao_login(self: object) -> None:
         """
-        Usa o retorno da função "procura_botao_login" e
-        clica no elemento que ela retorna
+        clicar no botão que leva a tela de login
         """
-        botão_para_login: webdriver.Firefox = self.procura_botao_login()
-        botão_para_login.click()
+        element: webdriver.Firefox = self.__driver.find_elements(By.TAG_NAME, "div")
+        for el in element:
+            if el.text == "Login":
+                botao_para_login = el
+                break
+        botao_para_login.click()
     
     def escrever_dados(self: object) -> None:
         """
@@ -139,102 +85,171 @@ class Bot(Usuario):
         element: webdriver = self.__driver.find_element(By.CLASS_NAME ,"lms-StandardLogin_LoginButton ")
         element.click()
        
-    def procurar_botao_menu(self: object) -> None:
-        element: webdriver.Firefox = self.__driver.find_elements(By.TAG_NAME, "div")
-        for el in element:
-            if el.get_attribute("class") == "hm-MainHeaderMembersWide_MembersMenuIcon ":
-                return el
-       
     def menu_clica(self: object) -> None:
-        menu: webdriver = self.procurar_botao_menu
-        if menu:
-            menu.click()
+        """
+        Procura butão de menu para acessar a barra de pesquisa
+        Usado quando a tela está pequena e a barra de pesquisa se esconde no menu
+        Clica no botão de menu
+        """
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for el in element:
+            if el.get_attribute("class") == "hm-MainHeaderLHSNarrow_AZIcon ":
+                el.click()
+                break
        
-    def pesquisar_no_menu(self: object) -> None:
-        element: webdriver.Firefox = self.__driver.find_elements(By.TAG_NAME, "div")
+    def pesquisar_menu(self: object) -> None:
+        """clica na caixa de pesquisa dentro do menu"""
+        element = self.__driver.find_elements(By.TAG_NAME, "input")
         for el in element:
             if el.get_attribute("class") == "wn-SiteSearch_SearchInput ":
                 el.click()
                 break
-       
-    def procurar_botao_pesquisar(self: object) -> webdriver.Firefox:
-        """Procura o botão que abre a caixa de pesquisa e retorna um o elemento em objeto webdrive"""
-        element: webdriver = self.__driver.find_elements(By.TAG_NAME, "div")
-        for el in element:
-            if el.get_attribute("class") == "hm-SiteSearchIconLoggedIn ":
-                return el
-        
-    def clicar_botao_pesquisar(self: object) -> None:
-        """Clica no botao de pesquisa"""
-        botao_pesquisar: webdriver.Firefox = self.procurar_botao_pesquisar()
-        botao_pesquisar.click()
-        
-    def procurar_caixa_pesquisa(self: object) -> webdriver.Firefox:
-        """Procura a caixa que será inserido a pesquisa"""
         element = self.__driver.find_elements(By.TAG_NAME, "input")
         for el in element:
             if el.get_attribute("class") == "sml-SearchTextInput ":
-                return el
+                var = el
+                break
+        var.send_keys(self.time)
         
-    def pesquisa(self: object) -> None:
-        """Escreve na caixa de pesquisa o time do objeto"""
-        caixa_pesquisa: webdriver.Firefox = self.procurar_caixa_pesquisa()
-        caixa_pesquisa.send_keys(self.time)
+    def pesquisar(self: object) -> None:
+        """Escreve na caixa de pesquisa o time escolhido"""
+        element: webdriver = self.__driver.find_elements(By.TAG_NAME, "div")
+        for el in element:
+            if el.get_attribute("class") == "hm-SiteSearchIconLoggedIn ":
+                el.click()
+                break
+        element = self.__driver.find_elements(By.TAG_NAME, "input")
+        for el in element:
+            if el.get_attribute("class") == "sml-SearchTextInput ":
+                var = el
+                break
+        var.send_keys(self.time)
         
     def capturar_jogos(self: object) -> list:
-        """Captura na tela os jogos referente a pesquisa e os retorna em uma lista"""
-        lista_jogos: list = []
+        """Captura na tela os jogos referente a pesquisa e os retorna em uma lista de webdriver"""
+        lista_jogos = list()
         element = self.__driver.find_elements(By.TAG_NAME, "div")
         for el in element:
             if el.get_attribute("class") == "ssm-SiteSearchLabelOnlyParticipant gl-Market_General-cn1 ":
                 jogo = el.find_element(By.TAG_NAME ,"span")
                 lista_jogos.append(jogo)
-        return lista_jogos 
+        return lista_jogos
     
-    def capturar_jogos_string(self: object):
-        jogos: list = self.capturar_jogos_string()
-        lista_jogos: list = []
-        for jogo in jogos:
-            var_jogo = jogo.text
-            lista_jogos.append(var_jogo)
-    
-    def escolher_jogo(self: object, escolha: int) -> str:
-        """
-        - O parametro "escolha" deverá vir do usuario.
+    def clicar_jogo(self: object, escolha: int) -> None:
+        """- O parametro "escolha" deverá vir do usuario.
         - "escolha" será o indice que indicará o jogo escolhido
-        - Será retornado o jogo escolhido em formato string
         """
-        jogos = self.capturar_jogos()
-        return jogos[escolha]
-    
-    def clicar_jogo(self: object) -> None:
-        jogo: webdriver.Firefox = self.escolher_jogo()
-        jogo.click()
-    
-    def separar_times(self: object) -> list:
-        jogo: webdriver.Firefox = self.escolher_jogo()
-        var = jogo.text
-        var_jogo = var.split()
-        del(var_jogo[1])
-        return var_jogo
-        
-    def lado_para_apostar(self: object) -> None:
-        ...
+        lista_jogos = list()
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for el in element:
+            if el.get_attribute("class") == "ssm-SiteSearchLabelOnlyParticipant gl-Market_General-cn1 ":
+                jogo = el.find_element(By.TAG_NAME ,"span")
+                lista_jogos.append(jogo)
+        lista_jogos[escolha].click()
+      
+    def clicar_time(self: object) -> None:
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for elem in element:
+            if elem.get_attribute("class") == "gl-MarketGroup ":
+                elemento = elem
+                break
+        lista = elemento.find_elements(By.TAG_NAME, "div")
+        for l in lista:
+            if l.get_attribute("class") == "gl-Participant gl-Participant_General gl-Market_General-cn3 ":
+                if "Flamengo" in l.text:
+                    time_aposta = l
+                    break
+        time_aposta.click()
+                  
+    def escrever_valor_aposta(self: object) -> None:
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for elem in element:
+            if elem.get_attribute("class") == "qbs-StakeBox_StakeInput ":
+                butao_valor = elem
+                break
+        butao_valor.click()
+        janela_bot = auto.getWindowsWithTitle("bet365 - Apostas Desportivas Online - Mozilla Firefox")[0]
+        try:
+            janela_bot.restore()
+            janela_bot.maximize()
+            janela_bot.activate()
+        except:
+            ...
+        auto.write(self.valor)
         
     def apostar(self: object) -> None:
-        ...
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for elem in element:
+            if elem.get_attribute("class") == "qbs-PlaceBetButton_Wrapper":
+                elem.click()
+                break
     
+    def concluir_aposta(self: object) -> None:
+        """Conclui a aposta"""
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for elem in element:
+            if elem.get_attribute("class") == "qbs-NormalBetItem_Indicator ":
+                elem.click()
+                break
+    
+    def minhas_apostas(self: object) -> None:
+        """Clica em minhas apostas"""
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for elem in element:
+            if elem.get_attribute("class") == "hm-MainHeaderCentreWide_Link hm-HeaderMenuItemMyBets ":
+                elem.click()
+                break
+    
+    def encerra_aposta(self: object) -> None:
+        """Encerra a primeira aposta encontrada"""
+        element = self.__driver.find_elements(By.TAG_NAME, "div")
+        for elem in element:
+            if elem.get_attribute("class") == "myb-CloseBetButtonBase_WrapperInner ":
+                if "Encerrar Aposta" in elem.text:
+                    elem.click()
+                    elem.click()
+                    break
+    
+    def voltar_minhas_apostas(self: object) -> None:
+        """aperta no botão voltar em minhas apostas"""
+        self.__driver.back()
+    
+    def tela_cheia(self: object) -> bool:
+        tela = auto.getWindowsWithTitle("bet365 - Apostas Desportivas Online - Mozilla Firefox")[0]
+        very = tela.isMaximized
+        return very
+        
     def fechar(self: object) -> None:
-        self.__driver.close()
-    
-def iniciar() -> None:
-    
-    bot._init()
-    bot._init_url()
-    bot.clicar_botao_login()
+        self.__driver.close() 
+        
+if __name__ == '__main__':
+    from time import sleep
+    bot = Bot("Kele51", "3122477", "Flamengo", "5")
+    bot.iniciar()
+    bot.abrir_url()
+    sleep(2)
+    bot.clica_botao_login()
     bot.escrever_dados()
     bot.fazer_login()
-
-    
-if __name__ == "__main__":
-    help(action_chains)
+    sleep(3)
+    x = input("- ")
+    #bot.menu_clica()
+    sleep(2)
+    #bot.pesquisar_menu()
+    bot.pesquisar()
+    sleep(2)
+    jogos = bot.capturar_jogos()
+    bot.clicar_jogo(2)
+    sleep(2)
+    bot.clicar_time()
+    sleep(1)
+    bot.escrever_valor_aposta()
+    bot.apostar()
+    sleep(1)
+    bot.concluir_aposta()
+    sleep(1)
+    bot.minhas_apostas()
+    sleep(2)
+    bot.encerra_aposta()
+    sleep(1)
+    bot.voltar_minhas_apostas()
